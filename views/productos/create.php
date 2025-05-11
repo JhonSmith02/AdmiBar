8dfb35aafe11cbee7089a77739e219a2442acd93
VanHohenheim22 <116129051+VanHohenheim22@users.noreply.github.com>
2025-05-10 21:02:24 -0500

    Se agregan estilos a productos create, se crean nuevas variables globales y se modifica la estructura de salida del html de create.php para facilitar el aplicar estilos

--- views/productos/create.php
+++ views/productos/create.php
@@ -8,17 +8,25 @@
     <?php include __DIR__ . '/formulario.php'; ?>
 </form>
 
-<?php foreach($errors as $row): ?>
-    <div class="alerta error" style=" background-color:red; ">
-        <?php echo $row; ?>
-    </div>
-<?php endforeach; ?>
+<div class="mensaje-contenedor">
+    <?php if(!empty($errors)): ?>
+        <div class="list-errores">
+            <?php foreach($errors as $error): ?>
+                <div class="msg error">
+                    <span class="mensaje-texto"><?php echo htmlspecialchars($error); ?></span>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    <?php endif; ?>
 
-<!-- Mensaje de exito que llega del modelo -->
-<?php $msg = $_GET['msg'] ?? null; ?>
-
-<?php if (intval($msg) === 1): ?> 
-    <span class="alerta exito" style="background-color: green;">Producto Creado Correctamente</span>
-<?php elseif (intval($msg) === 2): ?>
-    <span class="alerta exito">Producto Actualizado</span>
-<?php endif; ?>
+    <?php $msg = $_GET['msg'] ?? null; ?>
+    <?php if (intval($msg) === 1): ?> 
+        <div class="msg exito">
+            <span class="mensaje-texto">Producto Creado Correctamente</span>
+        </div>
+    <?php elseif (intval($msg) === 2): ?>
+        <div class="msg exito">
+            <span class="mensaje-texto">Producto Actualizado</span>
+        </div>
+    <?php endif; ?>
+</div>
